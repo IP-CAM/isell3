72e15975df1d3e44ad34789aa825dc0e56f5c420
baycik <baycik@gmail.com>
2015-01-22 21:38:57 +0400

    Update Maintain.php

--- application/models/Maintain.php
+++ application/models/Maintain.php
@@ -1,43 +1,69 @@
 <?php
 
-class Maintain extends CI_Model{
+class Maintain extends CI_Model {
 
-    public function Maintain(){
-	$this->zipPath='E:\update.zip';
-	$this->zipSubFolder='/isell3-master/';
-	$this->unpackDir='E:\unpack';
-	$this->workDir='E:\isell3';
+    public function Maintain() {
+	$this->dirParent=realpath('..');
+	$this->dirWork = realpath('.');
+	if( file_exists($this->dirWork.'/.git') ){
+	    exit("Work folder contains .git folder. Update may corrupt your work!");
+	}	
+	$this->dirWork = $this->dirParent.'/newisell3';//realpath('.');
+	$this->dirUnpack=$this->dirParent.'/isell3_update';
+	$this->dirBackup=$this->dirParent.'/isell3_backup';
+	$this->zipPath = $this->dirUnpack.'/isell3_update.zip';
+	$this->zipSubFolder = '/isell3-master/';
 	parent::__construct();
+    }
+
+    public function autoUpdate( $step='' ){
+	if( !$step ){
+	    header("Refresh: 2; url= download");
+	    return  'start download';
+	}
+	if( $this->appUpdate($step) ){
+	    if( $step=='download' ){
+		header("Refresh: 2; url= unpack");
+		return  'downloaded';
+	    }
+	    if( $step=='unpack' ){
+		header("Refresh: 2; url= swap");
+		return  'unpacked';
+	    }
+	    if( $step=='swap' ){
+		return  'update succeded!';
+	    }
+	}
+	else{
+	    return "Failure at step: $step";
+	}
     }
     
     public function appUpdate($action = 'download') {
-	echo realpath ( '../' );
-	exit;
-	
-	
 	if ($action == 'download') {
-	    return $this->updateDownload( BAY_UPDATE_URL, $this->zipPath );
+	    return $this->updateDownload(BAY_UPDATE_URL, $this->zipPath);
 	}
-	if ($action == 'unpack' ){
+	if ($action == 'unpack') {
 	    return $this->updateUnpack();
 	}
-	if ($action == 'swap'){
+	if ($action == 'swap') {
 	    return $this->updateSwap();
 	}
-	if ($action == 'init'){
+	if ($action == 'init') {
 	    
 	}
     }
 
     private function updateDownload($updateUrl, $updateFile) {
 	set_time_limit(240);
-	return copy ( $updateUrl , $updateFile );
+	mkdir($this->dirUnpack);
+	return copy($updateUrl, $updateFile);
     }
 
     private function updateUnpack() {
 	$zip = new ZipArchive;
-	if ($zip->open( $this->zipPath ) === TRUE) {
-	    $zip->extractTo($this->unpackDir);
+	if ($zip->open($this->zipPath) === TRUE) {
+	    $zip->extractTo($this->dirUnpack);
 	    $zip->close();
 	    return true;
 	} else {
@@ -45,10 +71,23 @@ class Maintain extends CI_Model{
 	}
     }
 
-    private function updateSwap(){
-	rename( $this->workDir , date("Y-m-d-H-i-s-").$this->workDir );
-	rename( $this->unpackDir.$this->zipSubFolder , $this->workDir );
-	unlink($this->unpackDir);
+    private function updateSwap() {
+	$this->delTree($this->dirBackup);
+	rename($this->dirWork, $this->dirBackup);
+	rename($this->dirUnpack . $this->zipSubFolder, $this->dirWork);
+	$this->delTree($this->dirUnpack);
+	return true;
     }
-    
+
+    private function delTree($dir) {
+	if( !file_exists ($dir) ){
+	    return false;
+	}
+	$files = array_diff(scandir($dir), array('.', '..'));
+	foreach ($files as $file) {
+	    (is_dir("$dir/$file")) ? $this->delTree("$dir/$file") : unlink("$dir/$file");
+	}
+	return rmdir($dir);
+    }
+
 }
