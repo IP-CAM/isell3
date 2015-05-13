e2223f4a7d9f5114e83df581c895ede5af6e9749
Baycik <Baycik@BaycikHome-tavrid390y7>
2015-05-13 21:19:36 +0400

    update Database

--- update_3.1.sql
+++ update_3.1.sql
@@ -1,35 +1,4 @@
-ALTER TABLE `nilua`.`acc_tree` 
-DROP COLUMN `top_id`,
-ADD COLUMN `acc_code` INT(10) UNSIGNED  NOT NULL AFTER `path`,
-ADD COLUMN `acc_type` VARCHAR(2) NULL AFTER `acc_code`,
-ADD COLUMN `acc_role` VARCHAR(45) NULL AFTER `acc_type`,
-ADD COLUMN `curr_id` INT UNSIGNED NULL AFTER `acc_role`,
-ADD COLUMN `is_favorite` TINYINT(1) NULL AFTER `curr_id`,
-ADD COLUMN `use_clientbank` TINYINT(1) NULL AFTER `is_favorite`,
-ADD INDEX `curr_code_idx` (`curr_id` ASC);
-ALTER TABLE `nilua`.`acc_tree` 
-ADD CONSTRAINT `curr_code`
-  FOREIGN KEY (`curr_id`)
-  REFERENCES `nilua`.`curr_list` (`curr_id`)
-  ON DELETE NO ACTION
-  ON UPDATE NO ACTION;
-
-update acc_tree set acc_code=null;
-
-ALTER TABLE `nilua`.`acc_tree` 
-CHANGE COLUMN `acc_code` `acc_code` INT(10) UNSIGNED NULL ,
-ADD UNIQUE INDEX `acc_code_UNIQUE` (`acc_code` ASC);
-
-ALTER TABLE `nilua`.`acc_trans` 
-ADD CONSTRAINT `cred_code`
-  FOREIGN KEY (`acc_credit_code`)
-  REFERENCES `nilua`.`acc_tree` (`acc_code`)
-  ON DELETE NO ACTION
-  ON UPDATE NO ACTION;
-
-ALTER TABLE `nilua`.`acc_trans` 
-ADD CONSTRAINT `deb_code`
-  FOREIGN KEY (`acc_debit_code`)
-  REFERENCES `nilua`.`acc_tree` (`acc_code`)
-  ON DELETE NO ACTION
-  ON UPDATE NO ACTION;
+update acc_check_list set date=null where date='0000-00-00 00:00:00';
+update acc_check_list set assumption_date=null where assumption_date='0000-00-00 00:00:00';
+update acc_check_list set value_date=null where value_date='0000-00-00 00:00:00';
+update document_list set reg_stamp=null where reg_stamp='0000-00-00 00:00:00';
