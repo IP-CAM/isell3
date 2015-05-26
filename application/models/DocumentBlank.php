b7a54f898187fcc51b6f017d94cf8f293461c5b3
Baycik <Baycik@BaycikHome-tavrid390y7>
2015-05-26 08:46:54 +0400

    Бланк процессор

--- application/models/DocumentBlank.php
+++ application/models/DocumentBlank.php
@@ -42,7 +42,7 @@ class DocumentBlank extends DocumentCore {
         }
         return $avail_docs;
     }
-    public function createBlank( $view_type_id, $register_only = false ){
+    public function blankCreate( $view_type_id, $register_only = false ){
         $doc_type = $this->get_value("SELECT doc_type FROM document_view_types WHERE view_type_id='$view_type_id'");
 	$Document2=$this->Base->bridgeLoad('Document');
         $Document2->add($doc_type);
@@ -53,4 +53,24 @@ class DocumentBlank extends DocumentCore {
         $this->Base->svar('selectedBlankId',$doc_id);
         return $doc_id;
     }
+    public function blankGet($doc_id) {
+        $this->Base->svar('selectedBlankId',$doc_id);
+	$Document2=$this->Base->bridgeLoad('Document');
+        $Document2->selectDoc($this->Base->svar('selectedBlankId'));
+        $blank = $this->get_row("SELECT * FROM document_view_list JOIN document_view_types USING(view_type_id) WHERE doc_id='$doc_id'");
+        if (!$blank) {//only registry record
+            $doc_type = $Document2->doc('doc_type');
+            $blank = $this->get_row("SELECT view_name FROM document_view_types WHERE doc_type='$doc_type'");
+        } elseif ($blank->html) {
+            $blank->html = stripslashes($blank->html);
+        } else {
+            $blank->html = file_get_contents('views/rpt/' . $blank->view_file, true);
+            $blank->loaded_is_tpl = true;
+        }
+        $blank->doc_num = $Document2->doc('doc_num');
+        $blank->doc_date = $Document2->doc('doc_date');
+        $blank->doc_data = $Document2->doc('doc_data');
+        return $blank;
+    }
+
 }
