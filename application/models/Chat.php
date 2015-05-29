df96b4e340f2fed0cd8b170cf641f033e62fa202
baycik <baycik@baycikNote>
2015-05-29 18:30:13 +0300

    Работа над чатом

--- application/models/Chat.php
+++ application/models/Chat.php
@@ -6,16 +6,16 @@ class Chat extends Catalog{
                 user_id,
                 user_login
             FROM
-                user_list";
+                " . BAY_DB_MAIN . ".user_list";
         return $this->get_list($sql);
     }
     public function sendRecieve( $counterpart='all', $msg=null ){
+        $counterpart=$this->db->escape($counterpart);
         if( $counterpart && $msg ){
-            $counterpart=$this->db->escape($counterpart);
             $msg=$this->db->escape(rawurldecode($msg));
             $this->addMessage($counterpart, $msg);
         }
-        return $this->getMessages();
+        return $this->getMessages($counterpart);
     }
     private function addMessage( $counterpart, $msg ){
         $user_id = $this->Base->svar('user_id');
@@ -30,10 +30,24 @@ class Chat extends Catalog{
                 event_is_private=1";
         $this->db->query($sql);
     }
-    private function getMessages(){
-        $user_login = $this->Base->svar('user_login');
-        $user_id = $this->Base->svar('user_id');
-        $sql="SELECT *,event_target='$user_login' me FROM event_list WHERE event_label='Chat' AND (event_user_id='$user_id' OR event_target='$user_login')";
+    private function getMessages( $he ){
+        $me = $this->db->escape($this->Base->svar('user_login'));
+        $sql="SELECT
+            event_list.*,
+            event_target=$me is_me,
+            event_target reciever,
+            user_login sender
+                FROM
+                    event_list
+                        JOIN
+                    " . BAY_DB_MAIN . ".user_list ON event_user_id=user_id
+                WHERE 
+                    event_label='Chat' 
+                HAVING
+                    IF($he='all',
+                        sender=$me OR reciever=$me,
+                        sender=$me AND reciever=$he OR sender=$he AND reciever=$me)
+                ORDER BY event_date";
         return $this->get_list($sql);
     }
 }
