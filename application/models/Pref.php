b2472891bce562e3b9ccfef8e9a0bc71c13625ab
Baycik <Baycik@BaycikHome-tavrid390y7>
2015-06-10 22:40:57 +0400

    added label field

--- application/models/Pref.php
+++ application/models/Pref.php
@@ -13,7 +13,8 @@ class Pref extends Catalog {
                     id_number,
                     id_given_by,
                     id_date,
-                    CONCAT(last_name,' ',first_name,' ',middle_name) AS full_name 
+                    CONCAT(last_name,' ',first_name,' ',middle_name) AS full_name,
+                    CONCAT(last_name,' ',first_name,' ',middle_name) AS label 
                 FROM 
 		    " . BAY_DB_MAIN . ".user_list
                 WHERE 
