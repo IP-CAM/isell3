7984219fab3bae93cd5ff13dec0a1c200224f260
Baycik <Baycik@BaycikHome-tavrid390y7>
2015-08-17 20:14:11 +0400

    working under trans_names

--- application/models/AccountsData.php
+++ application/models/AccountsData.php
@@ -1,6 +1,7 @@
 <?php
 require_once 'AccountsCore.php';
 class AccountsData extends AccountsCore{
+    public $min_level=2;
     public function transNameListFetch($selected_acc=null){
 	$having=$this->decodeFilterRules();
 	$this->check($selected_acc);
@@ -20,11 +21,24 @@ class AccountsData extends AccountsCore{
 	return $this->get_list($sql);
     }
     public function transNameUpdate($trans_type,$field,$value){
+	$this->Base->set_level(3);
 	$this->check($trans_type,'[a-z0-9_]*');
 	$this->check($field,'[a-z0-9_]*');
 	$this->check($value);
 	$type=  explode('_', $trans_type);
 	$this->query("UPDATE acc_trans_names SET $field='$value' WHERE acc_debit_code='$type[0]' AND acc_credit_code='$type[1]'");
+	return $this->db->affected_rows();
+    }
+    public function transNameCreate($acc_debit_code,$acc_credit_code){
+	$this->Base->set_level(3);
+	$this->check($acc_debit_code);
+	$this->check($acc_credit_code);
+	return $this->query("INSERT INTO acc_trans_names SET acc_debit_code='$acc_debit_code', acc_credit_code='$acc_credit_code', trans_name='---', user_level=3");
+    }
+    public function transNameDelete($trans_type){
+	$this->check($trans_type);
+	$dc=  explode('_', $trans_type);
+	$this->query("DELETE FROM acc_trans_names WHERE acc_debit_code='$dc[0]' AND acc_credit_code='$dc[1]'");
 	return $this->db->affected_rows();
     }
     public function accountTreeFetch( $parent_id = null ) {
