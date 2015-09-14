d02e4e8c40a7cac78582c60c0d8308e1dc1552cc
Baycik <Baycik@192.168.3.2>
2015-09-14 23:35:44 +0400

    adding backup manager

--- application/models/Maintain.php
+++ application/models/Maintain.php
@@ -126,19 +126,64 @@ class Maintain extends CI_Model {
 	}
 	return date ("Y-m-d\TH:i:s\Z", filemtime($this->dirWork));
     }
-    public function getDbBackup(){
+    private $path_to_backup_folder="/ISELL-DB-BACKUP/";
+    public function backupDump(){
         $this->Base->set_level(4);
-        $path_to_backup_folder="/ISELL-DB-BACKUP/";
         $path_to_mysql=$this->db->query("SHOW VARIABLES LIKE 'basedir'")->row()->Value;
-	if( !file_exists ($path_to_backup_folder) ){
-	    mkdir($path_to_backup_folder);
+	if( !file_exists ($this->path_to_backup_folder) ){
+	    mkdir($this->path_to_backup_folder);
 	}
-        $filename=$path_to_backup_folder.date('Y-m-d_H-i-s').'-ISELL-DB-BACKUP.sql';
-        exec("$path_to_mysql/bin/mysqldump --user=".BAY_DB_USER." --password=".BAY_DB_PASS." --single-transaction  ".BAY_DB_NAME." >".$filename,$output);
+        $filename=$this->path_to_backup_folder.date('Y-m-d_H-i-s')."-".BAY_DB_NAME.'-ISELL-DB-BACKUP.sql';
+        exec("$path_to_mysql/bin/mysqldump --user=".BAY_DB_USER." --password=".BAY_DB_PASS."  --default-character-set=utf8 --single-transaction=TRUE --routines --events  ".BAY_DB_NAME." >".$filename,$output);
         if( count($output) ){
             file_put_contents($filename.'.log', implode( "\n", $output ));
             return false;
         }
-        return $filename;
+        return true;
+    }
+    public function backupImport( $file ){
+	$this->Base->set_level(4);
+        $path_to_mysql=$this->db->query("SHOW VARIABLES LIKE 'basedir'")->row()->Value;
+	if( file_exists($this->path_to_backup_folder.$file) ){
+	    //echo "$path_to_mysql/bin/mysql --user=".BAY_DB_USER." --password=".BAY_DB_PASS." --single-transaction ".BAY_DB_NAME." <".$this->path_to_backup_folder.$file;
+	    exec("$path_to_mysql/bin/mysql --user=".BAY_DB_USER." --password=".BAY_DB_PASS." ".BAY_DB_NAME." <".$this->path_to_backup_folder.$file." 2>&1",$output);
+	    if( count($output) ){
+		file_put_contents($this->path_to_backup_folder.date('Y-m-d_H-i-s').'-IMPORT.log', implode( "\n", $output ));
+	    }
+	}
+    }
+    public function backupList(){
+	$this->Base->set_level(4);
+	$files = array_diff(scandir($this->path_to_backup_folder), array('.', '..'));
+	arsort($files);
+	return array_values ($files);
+    }
+    public function backupListNamed(){
+	$this->Base->set_level(4);
+	$named=[];
+	$list=$this->backupList();
+	foreach($list as $file){
+	    $named[]=['file'=>$file];
+	}
+	return $named;
+    }
+    public function backupDown( $file ){
+	$this->Base->set_level(4);
+	if( file_exists ($this->path_to_backup_folder.$file) ){
+	    header('Content-type: application/force-download');
+	    header('Content-Disposition: attachment; filename="'.$file.'"');
+	    echo file_get_contents($this->path_to_backup_folder.$file);
+	} else {
+	    show_error('X-isell-error: File not found!'.$this->path_to_backup_folder.$file, 404);
+	}
+    }
+    public function backupUp(){
+	if( $_FILES['upload_file'] ){
+	    return move_uploaded_file ( $_FILES['upload_file']["tmp_name"] , $this->path_to_backup_folder.$_FILES['upload_file']['name'] );
+	}
+    }
+    public function backupDelete( $file ){
+	$this->Base->set_level(4);
+	return unlink($this->path_to_backup_folder.$file);
     }
 }
