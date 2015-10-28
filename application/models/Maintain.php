7786cb7c340f049b995e32319d4b262fd0168316
Baycik <Baycik@192.168.3.2>
2015-10-28 22:10:45 +0400

    working on Maintain

--- application/models/Maintain.php
+++ application/models/Maintain.php
@@ -20,7 +20,7 @@ class Maintain extends CI_Model {
 	$this->dirUnpack=$this->dirParent.'/isell3_update';
 	$this->dirBackup=$this->dirParent.'/isell3_backup';
 	$this->zipPath = $this->dirUnpack.'/isell3_update.zip';
-	$this->zipSubFolder = '/isell3-master/';	
+	$this->zipSubFolder = $this->dirUnpack.'/isell3-master/';	
     }
     
     public function appUpdate($action = 'download') {
@@ -49,7 +49,7 @@ class Maintain extends CI_Model {
     }
 
     private function updateUnpack() {
-	$this->delTree($this->dirUnpack . $this->zipSubFolder);
+	$this->delTree($this->zipSubFolder);
 	$zip = new ZipArchive;
 	if ($zip->open($this->zipPath) === TRUE) {
 	    $zip->extractTo($this->dirUnpack);
@@ -60,30 +60,36 @@ class Maintain extends CI_Model {
 	}
     }
     
+//    private function safeRename( $old, $new ){
+//	$this->delTree($new);
+//	$atempt=10;
+//	while( $atempt-- ){
+//	    sleep(1);
+//	    if( rename($old,$new) ){
+//		return true;
+//	    }
+//	}
+//	return false;
+//    }
+    
     private function safeRename( $old, $new ){
-	$this->delTree($new);
-	$atempt=10;
-	while( $atempt-- ){
-	    sleep(1);
+	error_reporting(E_ERROR | E_WARNING | E_PARSE);
+	if( file_exists($old) ){
+	    $this->delTree($new);
 	    if( rename($old,$new) ){
 		return true;
+	    } else {
+		exec("move $old $new",$output,$code);
+		return $code==0;
 	    }
 	}
-	return false;
     }
     
     private function updateSwap() {
-	if( file_exists($this->dirWork)
-	    && file_exists($this->dirUnpack . $this->zipSubFolder)
-	    && file_exists($this->dirUnpack)){
-            
-	    $this->delTree($this->dirBackup);
-	    $this->safeRename($this->dirWork, $this->dirBackup);
-	    $this->safeRename($this->dirUnpack . $this->zipSubFolder, $this->dirWork);
-	    $this->delTree($this->dirUnpack);
-	    //$this->safeRename($this->dirBackup, $this->dirBackup.'_old');
-	    //$this->delTree($this->dirBackup.'_old');
-	    return true;
+	if( file_exists($this->dirWork) && file_exists($this->zipSubFolder) ){
+            return  $this->safeRename($this->dirWork, $this->dirBackup) && 
+		    $this->safeRename($this->zipSubFolder, $this->dirWork) &&
+		    $this->delTree($this->dirUnpack);
 	}
 	return false;
     }
@@ -119,6 +125,7 @@ class Maintain extends CI_Model {
 	$conf_file=$this->setupConf();
         $path_to_mysql=$this->db->query("SHOW VARIABLES LIKE 'basedir'")->row()->Value;
 	exec("$path_to_mysql/bin/mysql --defaults-file=$conf_file ".BAY_DB_NAME." <".$file." 2>&1",$output);
+	unlink($conf_file);
 	if( count($output) ){
 	    file_put_contents($this->path_to_backup_folder.date('Y-m-d_H-i-s').'-IMPORT.log', implode( "\n", $output ));
 	    return false;
