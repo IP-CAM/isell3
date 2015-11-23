4a8b38b7c4b3304a18a6b4f7fad4e91a5e395460
baycik <baycik@baycikNote>
2015-11-23 22:04:55 +0300

    working on clientbank

--- application/models/AccountsData.php
+++ application/models/AccountsData.php
@@ -109,4 +109,27 @@ class AccountsData extends AccountsCore{
 	    return $this->update('acc_tree',['is_favorite'=>$is_favorite],['acc_code'=>$acc_code]);
 	}
     }
+    public function clientBankGet( $main_acc_code=0, $page=1, $rows=30 ){
+        $this->check($page,'int');
+        $this->check($rows,'int');
+        $this->check($main_acc_code);
+	$active_company_id=$this->Base->acomp('company_id');
+        
+	$having=$this->decodeFilterRules();
+	$offset=$page>0?($page-1)*$rows:0;
+	$sql="SELECT *,
+		    IF(trans_id,'ok Проведен','gray Непроведен') AS status,
+		    IF(debit_amount,ROUND(debit_amount,2),'') AS debit,
+		    IF(credit_amount,ROUND(credit_amount,2),'') AS credit,
+		    DATE_FORMAT(transaction_date,'%d.%m.%Y') AS tdate
+                FROM acc_check_list 
+		WHERE main_acc_code='$main_acc_code' AND active_company_id='$active_company_id'
+		HAVING $having
+		ORDER BY transaction_date DESC 
+		LIMIT $rows OFFSET $offset";
+	$result_rows=$this->get_list($sql);
+	$total_estimate=$offset+(count($result_rows)==$rows?$rows+1:count($result_rows));
+        
+	return ['rows'=>$result_rows,'total'=>$total_estimate];
+    }
 }
