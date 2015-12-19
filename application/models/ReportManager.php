9b83412c232a7362c1821d203b4f6ecb3b3a7774
Baycik <Baycik@192.168.3.3>
2015-12-19 19:28:00 +0400

    working on new report module

--- application/models/ReportManager.php
+++ application/models/ReportManager.php
@@ -1,12 +1,14 @@
 <?php
-
-class ReportManager extends CI_Model {
+require_once 'Catalog.php';
+class ReportManager extends Catalog {
     private $plugin_folder='application/views/plugins/reports/';
     public function listFetch(){
 	$plugins=$this->scanFolder($this->plugin_folder);
 	$reports=[];
 	foreach($plugins as $plugin_folder){
-	    $reports[]=include $this->plugin_folder.$plugin_folder."/info.php";
+	    $info=include $this->plugin_folder.$plugin_folder."/info.php";
+	    $info['report_id']=$plugin_folder;
+	    $reports[]=$info;
 	}
 	return $reports;
     }
@@ -17,4 +19,11 @@ class ReportManager extends CI_Model {
 	return array_values($files);	
     }
     
+    public function formGet( $report_id=null ){
+	$this->check($report_id,'\w+');
+	if( $report_id ){
+	    return file_get_contents($this->plugin_folder.$report_id.'/form.html');
+	}
+	show_error('X-isell-error: Report id is not supplied!', 500);
+    }
 }
