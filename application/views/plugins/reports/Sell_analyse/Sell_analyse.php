65e0155e3005d845e4a1d29cead53453b1761587
Baycik <Baycik@192.168.3.5>
2015-12-20 23:41:13 +0400

    working on new report module

--- application/views/plugins/reports/Sell_analyse/Sell_analyse.php
+++ application/views/plugins/reports/Sell_analyse/Sell_analyse.php
@@ -6,7 +6,12 @@ class Sell_analyse extends Catalog{
     public function __construct() {
 	$this->idate=$this->dmy2iso( $this->request('idate','\d\d.\d\d.\d\d\d\d') );
 	$this->fdate=$this->dmy2iso( $this->request('fdate','\d\d.\d\d.\d\d\d\d') );
-	$this->all_active=$this->request('all_active','bool');	
+	$this->all_active=$this->request('all_active','bool');
+	$this->group_by_filter=$this->request('group_by_filter');
+	$this->group_by=$this->request('group_by','\w+');
+	if( !in_array($this->group_by, ['parent_id','analyse_type','analyse_group','analyse_class','analyse_section']) ){
+	    $this->group_by='parent_id';
+	}
 	parent::__construct();
     }
     private function dmy2iso( $dmy ){
@@ -16,16 +21,25 @@ class Sell_analyse extends Catalog{
     public function viewGet(){
 	$active_filter=$this->all_active?'':' AND active_company_id='.$this->Base->acomp('company_id');
 	$sql="SELECT 
-		*
+		(SELECT label FROM stock_tree WHERE branch_id=se.parent_id) cathegory,
+		IF($this->group_by='parent_id','',$this->group_by) group_by,
+		AVG( product_sell/product_quantity ) avg
 	    FROM
-		document_list
-		    JOIN
-		document_entries USING(doc_id)
+		stock_entries se
 		    JOIN
 		prod_list USING(product_code)
-	    WHERE 
-		cstamp>'$this->idate' AND cstamp<'$this->fdate'
-		AND doc_type=1 AND is_commited=1 $active_filter";
+		    JOIN
+		(SELECT 
+		    product_code,SUM(product_quantity) product_sell 
+		 FROM 
+		    document_list dl JOIN document_entries de USING(doc_id)
+		 WHERE 
+		    cstamp>'$this->idate' AND cstamp<'$this->fdate'
+		    AND doc_type=1 AND is_commited=1 $active_filter
+		 GROUP BY product_code) entries USING(product_code)
+	    GROUP BY
+		$this->group_by
+	    HAVING $this->group_by LIKE '%$this->group_by_filter%'";
 	$view=[
 		'rows'=>$this->get_list($sql)
 		];
