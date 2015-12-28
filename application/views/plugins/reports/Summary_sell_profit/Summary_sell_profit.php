8445228edbd881587ae1a2f3c5fec7754cc6c66a
Baycik <Baycik@192.168.3.2>
2015-12-28 23:35:24 +0400

    working on reports

--- application/views/plugins/reports/Summary_sell_profit/Summary_sell_profit.php
+++ application/views/plugins/reports/Summary_sell_profit/Summary_sell_profit.php
@@ -9,10 +9,11 @@ class Summary_sell_profit extends Catalog{
 	$this->all_active=$this->request('all_active','bool');
 	$this->count_reclamations=$this->request('count_reclamations','bool',0);
 	$this->in_alt_currency=$this->request('in_alt_currency','bool',0);
+	$this->language=$this->request('language','\w+','ru');
 	$this->group_by_filter=$this->request('group_by_filter');
 	$this->group_by=$this->request('group_by','\w+');
-	if( !in_array($this->group_by, ['parent_id','product_code','analyse_type','analyse_group','analyse_class','analyse_section']) ){
-	    $this->group_by='parent_id';
+	if( !in_array($this->group_by, ['label','product_code','analyse_type','analyse_group','analyse_class','analyse_section']) ){
+	    $this->group_by='label';
 	}
 	parent::__construct();
     }
@@ -24,66 +25,80 @@ class Summary_sell_profit extends Catalog{
 	$active_filter=$this->all_active?'':' AND active_company_id='.$this->Base->acomp('company_id');
 	$reclamation_filter=$this->count_reclamations?'':' AND is_reclamation=0';
         $having=$this->group_by_filter?"HAVING group_by LIKE '%$this->group_by_filter%'":"";
-        $sell_buy_table="
-            SELECT
-                product_code,
-                SUM( IF(doc_type=2,product_quantity,-product_quantity) ) stock_qty,
-                SUM( IF(doc_type=2,invoice_price/IF($this->in_alt_currency,doc_ratio,1)*product_quantity,0) )/SUM( IF(doc_type=2,product_quantity,0) ) buy_avg,
-                SUM( IF(doc_type=1 AND cstamp>'$this->idate',invoice_price/IF($this->in_alt_currency,doc_ratio,1)*product_quantity,0) ) sell_prod_sum,
-                SUM( IF(doc_type=1 AND cstamp>'$this->idate',product_quantity,0) ) sell_qty
-            FROM
-                document_entries de
-                    JOIN
-                document_list dl USING(doc_id)
-            WHERE
-                (doc_type=1 OR doc_type=2) AND cstamp<'$this->fdate' AND is_commited=1 $active_filter $reclamation_filter
-            GROUP BY product_code";
-        $sql="
-            SELECT 
-                IF('$this->group_by'='parent_id',(SELECT label FROM stock_tree WHERE branch_id=se.parent_id),$this->group_by) group_by,
-                SUM(sell_prod_sum) sell_sum,
-                SUM(buy_avg*stock_qty) stock_sum,
-                SUM(stock_qty) stock_sum_qty,
-                SUM(sell_qty) sell_sum_qty
-            FROM
-                stock_entries se
-                    JOIN
-                prod_list pl USING(product_code)
-                    LEFT JOIN
-                ($sell_buy_table) sellbuy USING(product_code)
-            GROUP BY
-		$this->group_by
-            $having";
-        //die($sql);
-        
-        $rows=$this->get_list($sql);
+	
+	$this->query("DROP TEMPORARY TABLE IF EXISTS tmp_sell_profit;");
+	$main_table_sql="CREATE TEMPORARY TABLE tmp_sell_profit ( INDEX(product_code) ) ENGINE=MyISAM AS (
+	    SELECT 
+		$this->group_by group_name,
+		product_code,
+		product_name,
+		sell_qty,
+		self_prod_sum/sell_qty self_avg,
+		self_prod_sum,
+		sell_prod_sum/sell_qty sell_avg,
+		sell_prod_sum,
+		sell_prod_sum-self_prod_sum net_prod_sum
+	    FROM 	    
+		(SELECT
+		    product_code,
+		    $this->language product_name,
+		    (SELECT label FROM stock_tree WHERE branch_id=se.parent_id) label,
+		    analyse_type,
+		    analyse_group,
+		    analyse_class,
+		    analyse_section,
+		    SUM( de.product_quantity ) sell_qty,
+		    SUM( de.self_price/IF($this->in_alt_currency,doc_ratio,1)*de.product_quantity ) self_prod_sum,
+		    SUM( de.invoice_price/IF($this->in_alt_currency,doc_ratio,1)*de.product_quantity ) sell_prod_sum
+		FROM
+		    document_entries de
+			JOIN
+		    document_list dl USING(doc_id)
+			JOIN
+		    stock_entries se USING(product_code)
+			JOIN
+		    prod_list pl USING(product_code)
+		WHERE
+		    doc_type=1 AND cstamp>'$this->idate' AND cstamp<'$this->fdate' AND is_commited=1 $active_filter $reclamation_filter
+		GROUP BY product_code) entries)";
+	$this->query($main_table_sql);
+	$rows=$this->get_list("SELECT * FROM tmp_sell_profit");
+	
+	$totals_table_sql="
+	    SELECT 
+		group_name,
+		SUM(sell_qty) sell_qty_sum,
+		SUM(self_prod_sum) self_sum,
+		SUM(sell_prod_sum) sell_sum,
+		SUM(sell_prod_sum)-SUM(self_prod_sum) net_sum
+	    FROM 
+		tmp_sell_profit
+	    GROUP BY group_name";
+	$totals=$this->get_list($totals_table_sql);
         $total_sell=0;
-        $total_sell_qty=0;
-        $total_stock=0;
-        $total_stock_qty=0;
-        foreach( $rows as $row ){
+        $total_self=0;
+        $total_net=0;
+        $total_qty=0;
+        foreach( $totals as $row ){
             $total_sell+=$row->sell_sum;
-            $total_sell_qty+=$row->sell_sum_qty;
-            $total_stock+=$row->stock_sum;
-            $total_stock_qty+=$row->stock_sum_qty;
-        }
-        foreach( $rows as $row ){
-            $row->sell_proc=    round( $row->sell_sum/$total_sell, 4);
-            $row->stock_proc=   round( $row->stock_sum/$total_stock, 4);
+            $total_self+=$row->self_sum;
+            $total_net+=$row->net_sum;
+            $total_qty+=$row->sell_qty_sum;
         }
 	function sort_bysell($a,$b){
-	    if( $a->sell_sum==$b->sell_sum ){
+	    if( $a->net_sum==$b->net_sum ){
 		return 0;
 	    }
-	    return ($a->sell_sum>$b->sell_sum)?-1:1;
+	    return ($a->net_sum>$b->net_sum)?-1:1;
 	}
-	usort($rows,'sort_bysell');
+	usort($totals,'sort_bysell');
 	$view=[
-                'total_sell'=>round($total_sell,2),
-                'total_stock'=>round($total_stock,2),
-                'total_sell_qty'=>round($total_sell_qty,2),
-                'total_stock_qty'=>round($total_stock_qty,2),
-		'rows'=>$rows
+                'total_sell'=>$total_sell,
+                'total_self'=>$total_self,
+                'total_net'=>$total_net,
+                'total_qty'=>$total_qty,
+		'rows'=>$rows,
+		'totals'=>$totals
 		];
 	return $view;	
     }
