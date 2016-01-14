33e5a15f0d1ca0b4f618977a42ed0f20fc561c6c
baycik <baycik@baycikNote>
2016-01-14 14:33:00 +0300

    Копейка в ПДВ

--- application/models/DocumentItems.php
+++ application/models/DocumentItems.php
@@ -41,8 +41,8 @@ class DocumentItems extends DocumentCore{
 		total_weight,
 		total_volume,
 		vatless,
-		total - vatless vat,
-		total,
+		ROUND(vatless*@vat_ratio,2) - vatless vat,
+		ROUND(vatless*@vat_ratio,2) total,
 		@curr_symbol curr_symbol,
 		self
 	    FROM
@@ -50,7 +50,6 @@ class DocumentItems extends DocumentCore{
 		    ROUND(SUM(product_quantity*product_weight),2) total_weight,
 		    ROUND(SUM(product_quantity*product_volume),2) total_volume,
 		    SUM(ROUND(product_quantity*invoice_price * @curr_correction,2)) vatless,
-		    ROUND(SUM(ROUND(product_quantity*invoice_price * @curr_correction,2) * @vat_ratio),2) total,
 		    SUM(ROUND(product_quantity*self_price,2)) self
 		FROM
 		    document_entries JOIN prod_list USING(product_code)
