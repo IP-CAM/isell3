8cfc4a37072c20914456da1cc54f37fa6d039057
Baycik <Baycik@192.168.3.4>
2016-01-17 13:33:35 +0400

    working on new events module

--- application/models/Events.php
+++ application/models/Events.php
@@ -1,7 +1,7 @@
 <?php
 require_once 'Catalog.php';
 class Events extends Catalog{
-    public $min_level=1;
+    public $min_level=2;
     /*
      * event_statuses
      * undone
@@ -16,12 +16,12 @@ class Events extends Catalog{
 	    FROM 
 		event_list 
 	    WHERE 
-		NOT event_is_private 
-		OR event_is_private AND (event_user_id='$user_id' OR $user_level>=3)
-	    ORDER BY event_date DESC
-	    ";
+		event_label<>'chat' 
+		AND ( NOT event_is_private OR event_is_private AND (event_user_id='$user_id' OR $user_level>=3) )
+	    ORDER BY event_date DESC";
 	return $this->get_list($sql);
     }
+    
     public function listFetch( $date ){
 	$this->check($date,'\d\d\d\d-\d\d-\d\d');
 	$sql="
@@ -32,7 +32,25 @@ class Events extends Catalog{
 	    FROM
 		event_list
 	    WHERE
-		DATE(event_date)='$date'";
+		DATE(event_date)='$date' AND event_label<>'chat'
+	    ORDER BY event_label";
 	return $this->get_list($sql);
+    }
+    
+    public function eventDelete( $event_id ){
+	$this->check($event_id,'int');
+	return $this->delete("event_list",['event_id'=>$event_id]);
+    }
+    
+    public function eventMove( $olddate, $newdate, $event_id, $label, $mode ){
+	$this->check($event_id,'int');
+	$this->check($olddate,'\d\d\d\d-\d\d-\d\d');
+	$this->check($newdate,'\d\d\d\d-\d\d-\d\d');
+	$this->check($label);
+	if( $mode=='all' ){
+	    $this->query("UPDATE event_list SET event_date='$newdate' WHERE DATE(event_date)='$olddate' AND event_label='$label'");
+	    return $this->db->affected_rows();
+	}
+	return $this->update('event_list',['event_date'=>$newdate],['event_id'=>$event_id]);
     }
 }
