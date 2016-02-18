742e257f3208b32a88176f4bf9deb977991c6fd6
Baycik <Baycik@192.168.3.3>
2016-02-18 22:19:46 +0400

    copy view role to dvl

--- application/models/proc/Document.php
+++ application/models/proc/Document.php
@@ -678,7 +678,7 @@ class Document extends Data {
 	    $efields = addslashes($this->getLastEfields($view_type_id));
 	}
 	$cstamp = $this->doc('cstamp');
-	$this->Base->query("INSERT INTO document_view_list SET doc_id='$doc_id', view_type_id='$view_type_id', view_efield_values='$efields', tstamp='$cstamp', view_num='$view_num'");
+	$this->Base->query("INSERT INTO document_view_list SET doc_id='$doc_id', view_type_id='$view_type_id', view_efield_values='$efields', tstamp='$cstamp', view_num='$view_num', view_role='{$view_type_props['view_role']}'");
 	return mysql_insert_id();
     }
     
--- install/db_update.sql
+++ install/db_update.sql
@@ -1,10 +1,16 @@
 ALTER TABLE `document_trans` 
-ADD COLUMN `label` VARCHAR(45) NULL DEFAULT NULL AFTER `type`;
+ADD COLUMN `trans_role` VARCHAR(45) NULL DEFAULT NULL AFTER `type`;
 
-UPDATE document_trans SET label='profit' WHERE `type`='791_441';
-UPDATE document_trans SET label='total' WHERE `type`='361_702' OR  `type`='28_631' OR  `type`='361_44' OR `type`='44_631';
-UPDATE document_trans SET label='vatless' WHERE `type`='702_791' OR  `type`='281_28' OR  `type`='44_441' OR `type`='441_44';
-UPDATE document_trans SET label='vat' WHERE `type`='702_641' OR  `type`='641_28' OR  `type`='44_641' OR `type`='641_44';
-UPDATE document_trans SET label='self' WHERE `type`='791_281';
+UPDATE document_trans SET trans_role='profit' WHERE `type`='791_441';
+UPDATE document_trans SET trans_role='total' WHERE `type`='361_702' OR  `type`='28_631' OR  `type`='361_44' OR `type`='44_631';
+UPDATE document_trans SET trans_role='vatless' WHERE `type`='702_791' OR  `type`='281_28' OR  `type`='44_441' OR `type`='441_44';
+UPDATE document_trans SET trans_role='vat' WHERE `type`='702_641' OR  `type`='641_28' OR  `type`='44_641' OR `type`='641_44';
+UPDATE document_trans SET trans_role='self' WHERE `type`='791_281';
+
+
+ALTER TABLE `document_view_list` 
+ADD COLUMN `view_role` VARCHAR(45) NULL DEFAULT NULL AFTER `freezed`;
+
+UPDATE document_view_list dvl JOIN document_view_types dvt USING(view_type_id) SET dvl.view_role=dvt.view_role;
 
 
