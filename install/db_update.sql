c4c04b5525f8d67c8374f1b79ec8a842e2c8fcc9
Baycik <Baycik@192.168.3.5>
2016-02-27 16:53:46 +0400

    working on service document support

--- install/db_update.sql
+++ install/db_update.sql
@@ -14,3 +14,39 @@ ADD COLUMN `view_role` VARCHAR(45) NULL DEFAULT NULL AFTER `freezed`;
 UPDATE document_view_list dvl JOIN document_view_types dvt USING(view_type_id) SET dvl.view_role=dvt.view_role;
 
 
+
+INSERT INTO `document_view_types` (`doc_type`, `view_name`, `view_tpl`) VALUES ('3', 'Акт выполенных работ', 'ua/doc/service_invoice.xlsx');
+INSERT INTO `document_view_types` (`doc_type`, `view_name`, `view_tpl`) VALUES ('4', 'Акт выполенных работ', 'ua/doc/service_invoice.xlsx');
+
+
+
+
+INSERT INTO `prod_list` (`product_code`, `ru`, `ua`, `en`, `is_service`,product_unit) VALUES ('аренда', 'Аренда помешений', 'Оренда приміщень', 'Rent for building', '1','м2');
+INSERT INTO `prod_list` (`product_code`, `ru`, `ua`, `en`, `is_service`,product_unit) VALUES ('топливо', 'Топливо', 'Паливо', 'Fuel', '1','л');
+INSERT INTO `prod_list` (`product_code`, `ru`, `ua`, `en`, `is_service`,product_unit) VALUES ('интернет', 'Интернет', 'Інтернет', 'Internet', '1','мес');
+INSERT INTO `prod_list` (`product_code`, `ru`, `ua`, `en`, `is_service`,product_unit) VALUES ('эл-во', 'Электроэнергия', 'Електроенергія', 'Electricity', '1','кВт*ч');
+INSERT INTO `prod_list` (`product_code`, `ru`, `ua`, `en`, `is_service`,product_unit) VALUES ('канц', 'Канц. товары', 'Канц. товари', 'Stationery', '1','шт');
+INSERT INTO `prod_list` (`product_code`, `ru`, `ua`, `en`, `is_service`,product_unit) VALUES ('офис', 'Материалы для офиса', 'Матеріали для офісу', 'Items for office', '1','шт');
+INSERT INTO `prod_list` (`product_code`, `ru`, `ua`, `en`, `is_service`,product_unit) VALUES ('телефон', 'Телефонная связь', 'Телефонний з`вязок', 'Phone', '1','мес');
+INSERT INTO `prod_list` (`product_code`, `ru`, `ua`, `en`, `is_service`,product_unit) VALUES ('ремонт', 'Ремонт авто или помещений', 'Ремонт авто чи приміщень', 'Repair of vehicle or office', '1','шт');
+INSERT INTO `prod_list` (`product_code`, `ru`, `ua`, `en`, `is_service`,product_unit) VALUES ('услуга', 'Услуга', 'Послуга', 'Service', '1','шт');
+
+INSERT INTO `stock_tree` (`label`, `path`) VALUES ( 'Услуги', '/Услуги/');
+SET @parent_id=LAST_INSERT_ID();
+UPDATE stock_tree SET top_id=@parent_id WHERE branch_id=@parent_id;
+
+
+INSERT INTO `stock_entries` (`product_code`, `parent_id`) VALUES ('аренда', @parent_id);
+INSERT INTO `stock_entries` (`product_code`, `parent_id`) VALUES ('топливо', @parent_id);
+INSERT INTO `stock_entries` (`product_code`, `parent_id`) VALUES ('интернет', @parent_id);
+INSERT INTO `stock_entries` (`product_code`, `parent_id`) VALUES ('эл-во', @parent_id);
+INSERT INTO `stock_entries` (`product_code`, `parent_id`) VALUES ('канц', @parent_id);
+INSERT INTO `stock_entries` (`product_code`, `parent_id`) VALUES ('офис', @parent_id);
+INSERT INTO `stock_entries` (`product_code`, `parent_id`) VALUES ('телефон', @parent_id);
+INSERT INTO `stock_entries` (`product_code`, `parent_id`) VALUES ('ремонт', @parent_id);
+INSERT INTO `stock_entries` (`product_code`, `parent_id`) VALUES ('услуга', @parent_id);
+
+
+
+
+
